58e478bf22725dc7726ebd37d05533c65a3b4d2e
James Pickering <james.pickering@xml-solutions.com>
2016-10-28 15:29:42 +0100

    Expand io_helpers, to include everything needed

--- mesh_client/io_helpers.py
+++ mesh_client/io_helpers.py
@@ -1,5 +1,10 @@
+from __future__ import division
 import io
+import itertools
+import mmap
+import os
 import zlib
+import six
 
 
 class GzipInputStream(object):
@@ -46,3 +51,80 @@ class GzipInputStream(object):
 
     def read_all(self):
         self.read()
+
+    def close(self):
+        if self._underlying is not None:
+            self._underlying.close()
+            self._underlying = None
+
+    def __del__(self):
+        self.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, typ, value, traceback):
+        self.close()
+
+
+class SplitFile(object):
+    def __init__(self, f, chunk_size=75 * 1024 * 1024):
+        self._fd = f.fileno()
+        self._chunk_size = ((chunk_size // mmap.ALLOCATIONGRANULARITY)
+                            * mmap.ALLOCATIONGRANULARITY)
+
+    def __len__(self):
+        return ((os.fstat(self._fd).st_size + self._chunk_size - 1)
+                // self._chunk_size)
+
+    def __iter__(self):
+        size = os.fstat(self._fd).st_size
+        for i in itertools.count():
+            if i * self._chunk_size >= size:
+                break
+            chunk_start = i * self._chunk_size
+            current_chunk_size = min(self._chunk_size, size - chunk_start)
+            m = mmap.mmap(self._fd, current_chunk_size,
+                          access=mmap.ACCESS_READ, offset=chunk_start)
+            try:
+                yield m
+            finally:
+                m.close()
+
+
+class SplitBytes(object):
+    def __init__(self, data, chunk_size=75 * 1024 * 1024):
+        self._data = data
+        self._chunk_size = chunk_size
+
+    def __len__(self):
+        return ((len(self._data) + self._chunk_size - 1)
+                // self._chunk_size)
+
+    def __iter__(self):
+        size = len(self._data)
+        for i in itertools.count():
+            if i * self._chunk_size >= size:
+                break
+            chunk_start = i * self._chunk_size
+            current_chunk_size = min(self._chunk_size, size - chunk_start)
+            yield io.BytesIO(
+                self._data[chunk_start:chunk_start + current_chunk_size])
+
+
+class CombineStreams(object):
+    def __init__(self, streams):
+        self._streams = iter(streams)
+        self._current_stream = six.next(self._streams)
+
+    def read(self, n=-1):
+        result = io.BytesIO()
+        try:
+            while True:
+                result.write(self._current_stream.read(n))
+                if n == -1 or n is None or result.tell() < n:
+                    self._current_stream = six.next(self._streams)
+                else:
+                    return result.getvalue()
+        except StopIteration:
+            return result.getvalue()
--- mesh_client/io_helpers_test.py
+++ mesh_client/io_helpers_test.py
@@ -2,7 +2,17 @@ from __future__ import absolute_import
 from unittest import TestCase, main
 import io
 import gzip
-from .io_helpers import GzipInputStream
+import six
+import tempfile
+from .io_helpers import GzipInputStream, SplitFile, SplitBytes, CombineStreams
+
+try:
+    from itertools import izip
+except ImportError:
+    izip = zip
+
+mebibyte = 1024 * 1024
+small_chunk = 10
 
 
 class IOHelpersTest(TestCase):
@@ -32,6 +42,89 @@ class IOHelpersTest(TestCase):
         # Decode with GzipFile
         test_decoder = gzip.GzipFile('', mode='r', fileobj=io.BytesIO(result))
         self.assertEqual(b"This is a short test stream", test_decoder.read())
+
+    def test_split_file(self):
+        with tempfile.TemporaryFile() as f:
+            f.write(b"a" * mebibyte)
+            f.write(b"b" * mebibyte)
+            f.flush()
+            instance = SplitFile(f, mebibyte)
+            self.assertEqual(len(instance), 2)
+            for m, c in izip(instance, [b"a", b"b"]):
+                self.assertEqual(m.read(mebibyte), c * mebibyte)
+
+    def test_split_file_irregular_size(self):
+        with tempfile.TemporaryFile() as f:
+            f.write(b"a" * mebibyte)
+            f.write(b"b")
+            f.flush()
+            instance = SplitFile(f, mebibyte)
+            self.assertEqual(len(instance), 2)
+            iterator = iter(instance)
+            chunk1 = six.next(iterator)
+            self.assertEqual(b"a" * mebibyte, chunk1.read(mebibyte))
+            chunk2 = six.next(iterator)
+            self.assertEqual(b"b", chunk2.read(mebibyte))
+
+    def test_split_file_irregular_size_2(self):
+        with tempfile.TemporaryFile() as f:
+            f.write(b"a" * mebibyte)
+            f.write(b"b" * (mebibyte - 1))
+            f.flush()
+            instance = SplitFile(f, mebibyte)
+            self.assertEqual(len(instance), 2)
+            iterator = iter(instance)
+            chunk1 = six.next(iterator)
+            self.assertEqual(b"a" * mebibyte, chunk1.read(mebibyte))
+            chunk2 = six.next(iterator)
+            self.assertEqual(b"b" * (mebibyte - 1), chunk2.read(mebibyte))
+
+    def test_split_bytes(self):
+        instance = SplitBytes(b"a" * small_chunk + b"b" * small_chunk,
+                              small_chunk)
+        self.assertEqual(len(instance), 2)
+        for m, c in izip(instance, [b"a", b"b"]):
+            self.assertEqual(c * small_chunk, m.read(small_chunk))
+
+    def test_split_bytes_irregular_size(self):
+        instance = SplitBytes(b"a" * small_chunk + b"b", small_chunk)
+        self.assertEqual(len(instance), 2)
+        iterator = iter(instance)
+        chunk1 = six.next(iterator)
+        self.assertEqual(b"a" * small_chunk, chunk1.read(small_chunk))
+        chunk2 = six.next(iterator)
+        self.assertEqual(b"b", chunk2.read(small_chunk))
+
+    def test_split_bytes_irregular_size_2(self):
+        instance = SplitBytes(b"a" * small_chunk + b"b" * (small_chunk - 1),
+                              small_chunk)
+        self.assertEqual(len(instance), 2)
+        iterator = iter(instance)
+        chunk1 = six.next(iterator)
+        self.assertEqual(b"a" * small_chunk, chunk1.read(small_chunk))
+        chunk2 = six.next(iterator)
+        self.assertEqual(b"b" * (small_chunk - 1), chunk2.read(small_chunk))
+
+    def test_combine_streams(self):
+        instance = CombineStreams(io.BytesIO(b"Hello") for i in range(20))
+        result = b""
+        for i in range(1000):
+            read_result = instance.read(i)
+            result += read_result
+            if len(read_result) < i:
+                break
+
+        self.assertEqual(instance.read(10), b"")
+        self.assertEqual(instance.read(), b"")
+        self.assertEqual(result, b"Hello" * 20)
+
+    def test_combine_streams_readall(self):
+        instance = CombineStreams(io.BytesIO(b"Hello") for i in range(20))
+        result = instance.read()
+
+        self.assertEqual(instance.read(10), b"")
+        self.assertEqual(instance.read(), b"")
+        self.assertEqual(result, b"Hello" * 20)
 
 if __name__ == '__main__':
     main()

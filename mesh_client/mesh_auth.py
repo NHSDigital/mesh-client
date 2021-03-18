df1057d13bc63d3ef5283d552162a7266aecdad2
James Pickering <james.pickering@airelogic.com>
2021-03-18 15:36:38 +0000

    Fix bytes/str confusion in mesh_auth helper

--- mesh_client/mesh_auth.py
+++ mesh_client/mesh_auth.py
@@ -5,13 +5,18 @@ from mesh_client.key_helper import get_shared_key_from_environ
 from argparse import ArgumentParser
 
 
+def encode_bytes(key):
+    return key.encode('utf-8')
+
+
 def main():
     parser = ArgumentParser(description="Create MESH auth token for user")
     parser.add_argument('user', help="The username")
     parser.add_argument('password', help="The password for the user")
     parser.add_argument('--shared-key',
                         help="The shared key to use for MESH authentication",
-                        default=get_shared_key_from_environ())
+                        default=get_shared_key_from_environ(),
+                        type=encode_bytes)
     args = parser.parse_args()
     generator = AuthTokenGenerator(args.shared_key, args.user, args.password)
     print(generator.generate_token())
